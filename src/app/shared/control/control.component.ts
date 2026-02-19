e252fd2d01f6519561980005b41ac97d767dad4b
oshevchuk0111 <o.shevchuk0111@gmail.com>
2026-02-19 21:29:53 +0100

    using host elements

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -6,8 +6,11 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+  },
 })
 export class ControlComponent {
-label = input.required<string>()
+  label = input.required<string>();
 }
